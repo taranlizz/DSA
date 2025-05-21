a778825fb7988e1938524e24c1f46beb9e1455c9
Yelyzaveta Taran <lizataran103@gmail.com>
2025-05-21 21:40:55 +0300

    max sum of subarray of size 'k'

---
+++ cpp/array/problems/subarray_sum_k.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+
+//Given an array of integers n and a positive number k, find the maximum sum of any contiguous subarray of size k.
+int subarray_sum_of_k(const std::vector<int>& n, const int k){
+     int window_sum = 0;
+     int max_sum = 0;
+
+     for(size_t i = 0; i < k; i++){
+       window_sum += n[i];
+     }
+
+     max_sum = window_sum;
+     for(size_t i = k; i < n.size(); i++){
+       window_sum += n[i] - n[i - k];
+       max_sum = std::max(window_sum, max_sum);
+     }
+     return max_sum;
+};
+
+
+
+int main(){
+    std::vector<int> test1 = {1, 9, -1, -2, 7, 3, -1,2};
+    int k1 = 4;
+    std::cout << subarray_sum_of_k(test1, k1) << std::endl;
+}
